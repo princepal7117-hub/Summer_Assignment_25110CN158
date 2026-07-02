15526cf34c0c380629d4c477b7a3aaca620375dc
princepal7117-hub <princepal7117@gmail.com>
2026-07-02 22:46:38 +0530

    added day 7 c programs

---
+++ day 7/ques25.c
@@ -0,0 +1,23 @@
+// wap to recursive factorial 
+
+#include <stdio.h>
+
+int factorial(int n);
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d = %d", num, factorial(num));
+
+    return 0;
+}
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
