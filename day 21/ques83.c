0e4b7395fb293b2bac6f8c0eafbf29021d72cad0
princepal7117-hub <princepal7117@gmail.com>
2026-07-04 13:04:32 +0530

    added day 21 c programs

---
+++ day 21/ques83.c
@@ -0,0 +1,32 @@
+/*Write a program to Count vowels and
+consonants.*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        if (isalpha(str[i])) {   // Check if character is an alphabet
+            char ch = tolower(str[i]);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        i++;
+    }
+
+    printf("Number of vowels = %d\n", vowels);
+    printf("Number of consonants = %d\n", consonants);
+
+    return 0;
+}
